d32d5bacfe58417af2e4157284b6cd0d540564d1
Nilanjana Jui <nilanjana.csecu@gmail.com>
2026-05-22 19:59:53 +0600

    remove unused import

--- src/app/explore-cars/page.jsx
+++ src/app/explore-cars/page.jsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState, useEffect, useContext } from "react";
+import { useState, useEffect } from "react";
 import { useAuth } from "@/context/AuthContext";
 import { useRouter } from "next/navigation";
 import Image from "next/image";
